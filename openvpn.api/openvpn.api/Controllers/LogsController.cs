31505e98e74fb8ccaf902f3e0608264ffd97ab88
Tomor Pupovci <tomorpupovci@gmail.com>
2014-11-17 16:54:41 +0100

    added API method to load all events...needs pagination and authorization

--- openvpn.api/openvpn.api/Controllers/LogsController.cs
+++ openvpn.api/openvpn.api/Controllers/LogsController.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using openvpn.api.common;
 using openvpn.api.core.controllers;
-
+using Raven.Client;
 
 namespace OpenVPN.Api.Controllers
 {
@@ -25,6 +25,21 @@ namespace OpenVPN.Api.Controllers
             await Session.StoreAsync(eventModel);
 
             return new HttpResponseMessage(HttpStatusCode.Created);
+        }
+        // TODO: Add pagination
+
+        public async Task<IHttpActionResult> Get()
+        {
+            IEnumerable<Event> logs = await Session.Query<Event>().ToListAsync();
+
+            return Ok(logs);
+
+        }
+        public async Task<IHttpActionResult> GetLog(string id)
+        {
+            Event log = await Session.LoadAsync<Event>(String.Format("{0}/{1}", "events", id));
+
+            return Ok(log);
         }
     }
 }
