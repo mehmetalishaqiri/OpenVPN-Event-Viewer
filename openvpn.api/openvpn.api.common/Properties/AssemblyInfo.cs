2d264411c9340dfef16bd5293cfc7e30ace13708
Mehmetali Shaqiri <mehmetalishaqiri@gmail.com>
2015-01-08 14:15:51 +0100

    common assembly info for all projects

--- openvpn.api/openvpn.api.common/Properties/AssemblyInfo.cs
+++ openvpn.api/openvpn.api.common/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("bc1d6c33-d78c-447f-9b56-33e46e45326d")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
